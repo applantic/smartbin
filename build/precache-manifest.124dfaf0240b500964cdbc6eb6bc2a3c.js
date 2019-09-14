ecba4732b8c79db33cfdb89120b23d318a3d5ff5
Kamil Dzieniszewski <kamil.dzieniszewski@vodeno.com>
2019-09-14 22:51:04 +0200

    add build

---
+++ build/precache-manifest.124dfaf0240b500964cdbc6eb6bc2a3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77d65cbecefa1d29857a9c5a0f9f3f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6467b47d222d145c861",
+    "url": "/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "e5675eba1120706e237f",
+    "url": "/static/css/main.461d90f5.chunk.css"
+  },
+  {
+    "revision": "d6467b47d222d145c861",
+    "url": "/static/js/2.1d9ba8b3.chunk.js"
+  },
+  {
+    "revision": "e5675eba1120706e237f",
+    "url": "/static/js/main.29b2eab6.chunk.js"
+  },
+  {
+    "revision": "bf8d023fd38ff913b53a",
+    "url": "/static/js/runtime~main.ba5b44fd.js"
+  },
+  {
+    "revision": "4605f750b62795bb66ea0980925fbb03",
+    "url": "/static/media/pointerIcon.4605f750.svg"
+  },
+  {
+    "revision": "9afe9c1e7e050fe4b37b2461003f9d97",
+    "url": "/static/media/smart-bin.9afe9c1e.svg"
+  }
+]);
