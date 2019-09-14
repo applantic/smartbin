1210799f92c300b7eb63be3909f708856fbcfb51
lukasz.liberadzki <lukasz.liberadzki@tuatara.pl>
2019-09-14 22:43:32 +0200

    Map progress

---
+++ src/components/SmartBinBigIcon.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SvgIcon } from '@material-ui/core';
+
+export default function SmartBinBigIcon(props) {
+  return (
+    <SvgIcon {...props} viewBox="0 0 116 142">
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M57.815 0C89.8357 0 115.63 26.2391 115.63 58.7045C115.63 75.1595 106.735 92.5041 83.1647 116.075L57.815 141.869L32.0206 116.075C8.89462 92.0593 0 74.7148 0 58.7045C0 26.2391 25.7944 0 57.815 0Z" fill="#29B574"/>
+      <rect x="37" y="41" width="35" height="60" fill="#F8F8F8"/>
+      <path d="M42 32L37 41H72L77 32H42Z" fill="white"/>
+      <path d="M71.9998 40.5308L76.9998 32L76.9998 91.2461L71.9998 101L71.9998 40.5308Z" fill="#D2D2D2"/>
+      <path d="M64.5 35.5C64.5 37.433 59.5899 38 56 38C52.4101 38 50 37.433 50 35.5C50 34 54.4101 33.5 58 33.5C61.5899 33.5 64.5 34.128 64.5 35.5Z" fill="#A2A2A2"/>
+      <path d="M54 33.4384L55.7627 30.8466L57.5255 28.2549L62.7091 31.7804L59.1836 36.964L58.3284 35.4347L56.5918 35.2012L56.1773 33.0239L54 33.4384Z" fill="#F4F4F4"/>
+      <path d="M67.2207 14L57.5255 28.2549L62.709 31.7804L72.4043 17.5255L67.2207 14Z" fill="#E8A16D"/>
+      <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="54" y="28" width="9" height="9">
+      <path d="M54 33.4384L55.7627 30.8466L57.5255 28.2549L62.7091 31.7804L59.1836 36.964L58.3284 35.4347L56.5918 35.2012L56.1773 33.0239L54 33.4384Z" fill="#F4F4F4"/>
+      </mask>
+      <g mask="url(#mask0)">
+      <rect x="58.0713" y="28.6818" width="0.691422" height="7.60564" transform="rotate(34.221 58.0713 28.6818)" fill="#F1F1F1"/>
+      <rect x="59.2147" y="29.4595" width="0.691422" height="7.60564" transform="rotate(34.221 59.2147 29.4595)" fill="#F1F1F1"/>
+      <rect x="60.3582" y="30.2372" width="0.691422" height="7.60564" transform="rotate(34.221 60.3582 30.2372)" fill="#F1F1F1"/>
+      <rect x="61.5016" y="31.0149" width="0.691422" height="7.60564" transform="rotate(34.221 61.5016 31.0149)" fill="#F1F1F1"/>
+      </g>
+      <rect x="67.7924" y="14.3889" width="0.691422" height="17.2856" transform="rotate(34.221 67.7924 14.3889)" fill="#E29D6B"/>
+      <rect x="68.9358" y="15.1665" width="0.691422" height="17.2856" transform="rotate(34.221 68.9358 15.1665)" fill="#E29D6B"/>
+      <rect x="70.0792" y="15.9442" width="0.691422" height="17.2856" transform="rotate(34.221 70.0792 15.9442)" fill="#E29D6B"/>
+      <rect x="71.2227" y="16.722" width="0.691422" height="17.2856" transform="rotate(34.221 71.2227 16.722)" fill="#E29D6B"/>
+    </SvgIcon>
+  );
+}
